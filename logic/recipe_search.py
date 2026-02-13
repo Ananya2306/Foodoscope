51b5ad2272ac74eafc0f0ba17d1e68afea07674c
Ananya2306 <ananyaarora230106@gmail.com>
2026-02-13 15:19:07 +0530

    testing

--- logic/recipe_search.py
+++ logic/recipe_search.py
@@ -1,1 +1,29 @@
-#Recipe filtering
+import requests
+
+API_KEY = "xPtdt8S0PblDqUJgxsi_So76_i2-LyTwG4XM8mSUROjFIx88"
+
+BASE_URL = "https://api.foodoscope.com/recipe2-api/recipe-bytitle"
+
+
+def search_recipe_by_title(title):
+    url = f"{BASE_URL}/recipeByTitle"
+
+    headers = {
+        "Authorization": f"Bearer {API_KEY}",
+        "Content-Type": "application/json"
+    }
+
+    params = {
+        "title": title
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+
+    print("Request URL:", response.url)
+    print("Status:", response.status_code)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Error:", response.text)
+        return None
---
+++ test.py
@@ -0,0 +1,5 @@
+from logic.recipe_search import search_recipe_by_title
+
+data = search_recipe_by_title("chicken curry")
+
+print(data)
